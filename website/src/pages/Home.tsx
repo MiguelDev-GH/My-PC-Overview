80401ecddeaa512219dd9e550a365e3d059486e3
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-03-03 08:55:05 -0300

    feat(style): new modifications in website homepage

--- website/src/pages/Home.tsx
+++ website/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import HomePic from "./../../../client/src/assets/PageInitTitle.png"
+import { Cpu, Gpu, MemoryStick, HardDrive, LayoutDashboard, Wifi } from 'lucide-react'
 import "./Home.css"
 
 const Home = () => {
@@ -13,36 +14,61 @@ const Home = () => {
           </div>  
         </div>
 
-        <div className="about">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, quod, quasi maxime quam, aliquam rem perferendis blanditiis cupiditate a ullam consectetur corporis? Nihil, voluptate non itaque sint repellendus voluptatum vel.
-        Accusantium quam, sint commodi dolor incidunt enim iure quis numquam minima a mollitia libero ipsum, est laudantium, illum adipisci eum quod unde et at rem! Ad quos voluptate fuga inventore.
-        Laudantium totam mollitia optio earum facilis quam adipisci eos obcaecati nobis harum. Qui veritatis dolorem minima obcaecati, eveniet nemo quidem eius amet impedit. Similique impedit doloremque necessitatibus ad dolorum repudiandae?
-        Quia at eos architecto iure quam ab quasi veritatis. Temporibus quia at id deleniti, error earum saepe tenetur quos reprehenderit doloremque, vel ab quisquam aliquam, repellendus minima similique quod nemo.
-        Corporis culpa blanditiis eveniet sapiente quidem, placeat ipsam sed ullam fugit quia hic aut! Iste voluptatibus, dignissimos quisquam rem harum commodi eveniet possimus, autem soluta culpa, molestiae velit doloribus facilis!
-        Laborum illo impedit inventore commodi. Sequi sit ex quisquam minus fuga eum hic quaerat debitis commodi id, labore nostrum quo minima, voluptate quam natus vitae. Eligendi magni quae suscipit soluta.
-        Praesentium excepturi aliquid numquam obcaecati, deserunt, voluptatum tempora voluptas maxime reprehenderit veniam possimus molestiae illum beatae sapiente fuga mollitia nostrum fugit reiciendis, suscipit vitae blanditiis minima! Dolore quo iusto natus?
-        Ab excepturi reprehenderit ipsum sed, asperiores cumque. Nam accusantium voluptas in voluptatem corporis tempore aperiam excepturi explicabo quia vel, neque voluptatum natus eligendi deleniti. Non voluptates deserunt eos perferendis explicabo!
-        Vitae, dicta voluptatum corporis in excepturi itaque eos veritatis! Ab quae voluptatibus cum, error quia molestias! Blanditiis quod eos pariatur nostrum, dignissimos odit ipsam, quam aspernatur esse nesciunt, voluptas odio?
-        Nesciunt doloribus quidem consequuntur magni dolores excepturi obcaecati perferendis tempora vitae laudantium adipisci accusantium debitis quia harum officia consequatur eum maiores eos, et voluptas repudiandae perspiciatis nemo. Officia, nam dignissimos.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laboriosam culpa ad hic corporis a sapiente voluptatem ipsam, saepe quod aperiam ducimus odit, eos aspernatur quam ut ex iure ipsa!
-        Provident rem mollitia repellat eum sapiente a veritatis, voluptatibus laborum quo exercitationem enim, quibusdam animi esse voluptas expedita? Rem voluptate incidunt sunt perferendis hic totam iusto dolores magni laboriosam deserunt!
-        Atque ut consequuntur laboriosam corporis adipisci quaerat nulla aut temporibus quam voluptates molestias iste sed dolor ea exercitationem quasi sit minus, enim sapiente neque, praesentium distinctio amet fugit placeat. Harum.
-        Quibusdam maxime nostrum tempore laboriosam sint perspiciatis esse voluptate voluptatibus optio ab, neque ex expedita quos tenetur repudiandae corporis odio incidunt. Numquam doloremque praesentium modi vero dolore. Laudantium, consectetur cupiditate.
-        Ipsam enim porro, sequi sed ab hic facere aliquid voluptatibus vero, temporibus numquam iste autem vitae accusantium delectus quam impedit unde esse quidem sint. Vel aliquid ducimus ex nemo debitis.
-        Minus voluptas inventore rerum. Aliquid voluptatem reiciendis adipisci cum laudantium aperiam ipsam est rem natus consequatur qui, a distinctio, quo debitis architecto tempora dolor ad, exercitationem corrupti. Fuga, illo qui.
-        Blanditiis eum eos quibusdam, commodi aliquam similique consequuntur beatae. Adipisci, vel, nam quos qui, asperiores officiis deserunt tenetur dolorem consequuntur aut non amet voluptatum aperiam! Obcaecati incidunt corporis deleniti minus?
-        Dolores corporis, animi labore maxime accusamus quos ut assumenda, sunt voluptate fuga ipsa ipsam, velit culpa? Quam nemo, quos, magnam inventore iure laborum animi iste facilis, enim cupiditate nihil officia!
-        Ut modi, commodi qui minus quis repellat laudantium consequuntur fugit iste expedita itaque sed ex quam, deleniti corrupti nemo, laborum voluptate error deserunt vitae. Repellendus maxime asperiores praesentium? Sit, aspernatur.
-        Doloribus, alias. Ex deserunt nobis sint esse beatae. Sit ratione at culpa sequi distinctio recusandae voluptatum enim, quis, cumque accusamus placeat explicabo tempora perspiciatis quisquam aliquam quam a officia voluptatem?
-        Praesentium, illum porro maiores laborum quia rem nam eveniet, sint magnam laudantium officiis, excepturi incidunt. Blanditiis minus voluptates expedita vitae animi laboriosam soluta numquam, perspiciatis aspernatur repudiandae nesciunt maxime aut!
-        Alias enim totam quisquam natus facere consequatur placeat accusantium nostrum ut reprehenderit, commodi velit. Voluptate at corrupti non, nemo repellat sed, doloribus error, impedit blanditiis aliquam sapiente neque. Magni, molestiae?
-        Reprehenderit nam, numquam tempora quia laboriosam ea et iure maxime voluptatem temporibus eaque optio voluptatum earum, consequuntur ut voluptates odio nobis facere quas quasi voluptatibus rerum deserunt. Itaque, provident nobis!
-        Quas quod explicabo quibusdam amet necessitatibus odio non optio deserunt ullam, nisi sint commodi modi quasi, eligendi vel delectus perferendis, eum magnam impedit recusandae? Quia hic officia amet maiores laborum.
-        Debitis veritatis velit commodi ducimus officiis beatae, harum eius sunt quo consequatur expedita natus, facere pariatur quibusdam, atque eligendi ipsum dolore exercitationem quam reprehenderit molestiae perferendis? Dicta incidunt eveniet obcaecati?
-        Ad quas aperiam corporis voluptatum expedita in, sed necessitatibus, nam natus perspiciatis soluta, unde eius ducimus fuga harum corrupti a ut commodi dolore neque consequuntur! Numquam beatae nesciunt recusandae itaque?
-        Praesentium, fugit odio ducimus deserunt expedita excepturi corrupti, maxime nemo quisquam eos culpa alias cumque sapiente quas quo, quis quae numquam doloremque ipsam sit? Incidunt tempora libero harum aut perspiciatis?
-        Ab exercitationem dignissimos beatae aperiam, aliquid, quibusdam ut ipsum sunt repellat magnam harum, veniam quis sit! Doloremque ipsa quaerat ullam inventore optio fugiat, eaque modi, sint, eum et laudantium ea!
-        Porro pariatur nam neque aut. Consequatur recusandae tempore nihil, veritatis eveniet, sit magni quasi a qui quod doloremque harum esse molestiae deleniti obcaecati. Rem harum velit sunt quae tempore earum!
-        Labore, tenetur et? Laboriosam nisi dolores tenetur, vel ratione quia labore inventore nam ipsa aliquam nostrum qui necessitatibus reprehenderit dolorum rerum tempora natus alias distinctio non maxime culpa mollitia cupiditate!</div>
+        <div className="about"> 
+          <div className="overview">
+            <strong>PC Overview </strong> 
+            is a powerful local application that extracts and displays detailed information about 
+            your computer's hardware and operating system. Built with modern web technologies, it 
+            provides a complete view of what's happening "under the hood" of your machine.
+
+            <br /><br />
+
+            With a fast Node.js server reading data directly from the system and a dynamic React.js 
+            interface displaying it all, you have access to component specifications, temperature monitoring, 
+            and real-time resource usage, all running offline and securely on your own PC.
+          </div>
+
+          <h2>What you can monitor</h2>
+
+          <div className="grids">
+              <div>
+                  <Cpu size={40} style={{color: '#02334b'}}/>
+                  <h3> Processor (CPU)</h3>
+                  <p>Detailed information about the model, cores, threads, and real-time monitoring of temperature and speed.</p>
+                </div>
+                <div>
+                  <Gpu size={40} style={{color: '#25631c'}}/>
+                  <h3> Graphics (GPU)</h3>
+                  <p>Data about your graphics cards, power limits, VRAM usage, temperatures, and fan speeds.</p>
+                </div>
+
+                <div>
+                    <MemoryStick size={40} style={{color: '#6f1d1d'}}/> 
+                    <h3>RAM Memory</h3>
+                    <p>Visualization of total system memory usage, plus physical details about each installed RAM stick (size, speed, brand).</p>
+                </div>
+
+                <div>
+                    <HardDrive size={40} style={{color: '#6f551d'}}/>
+                    <h3> Storage</h3>
+                    <p>Free and used space on all your disks and partitions, plus data on disk type (SSD/HDD) and health.</p>
+                </div>
+
+                <div>
+                    <LayoutDashboard size={40} style={{color: '#6f6f1d'}}/> 
+                    <h3>System & Motherboard</h3>
+                    <p>Operating System details, active users, motherboard manufacturer, BIOS model, and hardware limits.</p>
+                </div>
+
+                <div>
+                    <Wifi size={40} style={{color: '#1d6b6f'}}/>
+                    <h3> Connectivity</h3>
+                    <p>Status of your current network (Wi-Fi or Cable), local IPs (IPv4 and IPv6), and a list of all network interfaces.</p>
+                </div>
+
+          </div>
+
+        </div>
 
     </main>
     
