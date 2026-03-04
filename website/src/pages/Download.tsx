3b0a0b4251e50afd0abafb63a438f24b50b8098d
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-03-03 21:15:29 -0300

    fix(style): responsive

--- website/src/pages/Download.tsx
+++ website/src/pages/Download.tsx
@@ -2,10 +2,16 @@ import { DownloadIcon, ArrowDownToDot } from "lucide-react"
 import WindowsLogo from "../assets/windowsLogo.png"
 import MacOsLogo from "../assets/macOsLogo.png"
 import LinuxLogo from "../assets/linuxLogo.png"
-import Logo from "../../public/logo.png"
+import Logo from "../assets/logo.png"
 import "./Download.css"
 
 const Download = () => {
+
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+    });
+
     return (
         <main className="Download">
             <div className="home">
