26cccec3570545f613c3d42e9dc558be70b59399
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-03-07 00:36:38 -0300

    fix: error ´not using react header´

--- website/src/pages/documentation/errors.tsx
+++ website/src/pages/documentation/errors.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Errors = () => {
     return (
         <div className='content'>
--- website/src/pages/documentation/gettingstarted.tsx
+++ website/src/pages/documentation/gettingstarted.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const GettingStarted = () => {
     return (
         <div className='content'>
